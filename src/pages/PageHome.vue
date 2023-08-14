ceb0b7d6da403716d75483755ebdc0be45466340
Valensi <vallitriya@gmail.com>
2023-08-14 18:57:15 +0300

    mobile card

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,7 +1,9 @@
 <template>
   <q-page class="container q-pa-md">
     <q-card 
-      class="card-post bg-pink-4"
+      v-for="post in posts"
+      :key="post.id"
+      class="card-post bg-pink-4 q-mb-md"
       flat
       bordered>
       <q-item>
@@ -15,17 +17,17 @@
           <q-item-label
           class="text-bold text-white">valensimip</q-item-label>
           <q-item-label caption>
-            Saint Petersburg, Russia
+            {{post.location}}
           </q-item-label>
         </q-item-section>
       </q-item>
 
       <q-separator />
 
-      <img src="https://sun9-26.userapi.com/impg/lVE22o2WWEBMlSqCsMxkNYsWsUM_RuAL_PQy1g/GOVd4DuVAX4.jpg?size=640x640&quality=96&sign=127e6dd64e0cd1391e51245ee89af184&type=album">
+      <img :src="post.imageUrl">
       <q-card-section>
-        <div>Flowers</div>
-        <div class="text-caption text-white">August 10 18:09</div>
+        <div>{{post.caption}}</div>
+        <div class="text-caption text-white">{{post.date | niceDate}}</div>
         <div class="wrapper absolute-bottom-right">
           <q-btn flat round icon="favorite" />
           <q-btn flat round icon="bookmark" />
@@ -39,8 +41,49 @@
 </template>
 
 <script>
+import { date } from 'quasar'
+
 export default {
-  name: 'PageHome'
+  name: 'PageHome',
+  data(){
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: "Flowers",
+          date: 1692027508724,
+          location: "Saint Petersburg, Russia",
+          imageUrl: "https://sun9-26.userapi.com/impg/lVE22o2WWEBMlSqCsMxkNYsWsUM_RuAL_PQy1g/GOVd4DuVAX4.jpg?size=640x640&quality=96&sign=127e6dd64e0cd1391e51245ee89af184&type=album"
+        },
+        {
+          id: 2,
+          caption: "Flowers",
+          date: 1692027508724,
+          location: "Saint Petersburg, Russia",
+          imageUrl: "https://sun9-26.userapi.com/impg/lVE22o2WWEBMlSqCsMxkNYsWsUM_RuAL_PQy1g/GOVd4DuVAX4.jpg?size=640x640&quality=96&sign=127e6dd64e0cd1391e51245ee89af184&type=album"
+        },
+        {
+          id: 3,
+          caption: "Flowers",
+          date: 1692027508724,
+          location: "Saint Petersburg, Russia",
+          imageUrl: "https://sun9-26.userapi.com/impg/lVE22o2WWEBMlSqCsMxkNYsWsUM_RuAL_PQy1g/GOVd4DuVAX4.jpg?size=640x640&quality=96&sign=127e6dd64e0cd1391e51245ee89af184&type=album"
+        },
+        {
+          id: 4,
+          caption: "Flowers",
+          date: 1692027508724,
+          location: "Saint Petersburg, Russia",
+          imageUrl: "https://sun9-26.userapi.com/impg/lVE22o2WWEBMlSqCsMxkNYsWsUM_RuAL_PQy1g/GOVd4DuVAX4.jpg?size=640x640&quality=96&sign=127e6dd64e0cd1391e51245ee89af184&type=album"
+        }
+      ]
+    }
+  },
+  filters: {
+    niceDate(value){
+      return date.formatDate(value, 'MMM D h:mmA')
+    }
+  }
 }
 </script>
 <style lang="sass">
