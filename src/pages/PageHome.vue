7f08e01aaba2163cf9c29a5503be8b6d1d948d2b
Valensi <vallitriya@gmail.com>
2023-08-14 18:57:15 +0300

    mobile card

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -41,53 +41,8 @@
 </template>
 
 <script>
-import { date } from 'quasar'
 export default {
-  name: 'PageHome',
-  data(){
-    return {
-      posts: [
-        {
-          id: 1,
-          name: "valensimip",
-          caption: "Flowers",
-          date: 1692027508724,
-          location: "Saint Petersburg, Russia",
-          imageUrl: "https://sun9-26.userapi.com/impg/lVE22o2WWEBMlSqCsMxkNYsWsUM_RuAL_PQy1g/GOVd4DuVAX4.jpg?size=640x640&quality=96&sign=127e6dd64e0cd1391e51245ee89af184&type=album"
-        },
-        {
-          id: 2,
-          name: "wolfauf",
-          caption: "Flowers",
-          date: 1692040043379,
-          location: "Saint Petersburg, Russia",
-          imageUrl: "https://sun9-47.userapi.com/impg/fSZFsrqKZdh3Kp3AkfahU6lDdl1-RscB3AnGew/A1a6nZ7mU6s.jpg?size=750x1225&quality=96&sign=d7070d4bc6a14e5fc5edaf0528fafded&type=album"
-        },
-        {
-          id: 3,
-          caption: "Cake",
-          date: 1692040140951,
-          location: "Saint Petersburg, Russia",
-          imageUrl: "https://sun9-13.userapi.com/impg/gOKXlXiJu3XH7SGJvzX3TkSE0GmGS4QaTMALxA/3FpZKJBCo84.jpg?size=1080x1080&quality=96&sign=7bd8a9bf28aff869c3a2febe0d884257&type=album"
-        },
-        {
-          id: 4,
-          caption: "Water",
-          date: 1692027508724,
-          location: "Saint Petersburg, Russia",
-          imageUrl: "https://sun9-3.userapi.com/impg/RpkLkB-_YQ3KA-rRgysv4EfcDXVV_vNt1m1t6Q/PuVIZu3RyA0.jpg?size=636x636&quality=95&sign=38bf6d2ae2d377651be6123db7c97bd4&type=album"
-        }
-      ]
-    }
-  },
-  filters: {
-    niceDate(value){
-      return date.formatDate(value, 'MMM D h:mmA')
-    }
-  },
-  mounted(){
-    console.log(this)
-  }
+  name: 'PageHome'
 }
 </script>
 <style lang="sass">
