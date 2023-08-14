0a943ea0bbe212e7252be40b9fc061a4187bb8b7
Valensi <vallitriya@gmail.com>
2023-08-14 21:45:37 +0300

    laptop page home

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,57 +1,42 @@
 <template>
   <q-page class="container q-pa-md">
-    <div class="row q-col-gutter-lg">
-      <div class="col-12 col-sm-8">
-        <q-card 
-        v-for="post in posts"
-        :key="post.id"
-        class="card-post bg-pink-4 q-mb-md"
-        flat
-        bordered>
-        <q-item>
-          <q-item-section avatar>
-            <q-avatar>
-              <img src="https://sun9-48.userapi.com/impg/If3R-LcLYWlkmKukSnxnCFUm83Cf-aQm3ZDKCA/CElOBgUC0CU.jpg?size=1400x1936&quality=95&sign=a9a9f900f90dca64c7bab48e9786f66a&type=album">
-            </q-avatar>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label
-            class="text-bold text-white">{{post.name}}</q-item-label>
-            <q-item-label caption>
-              {{post.location}}
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-separator />
-        <img :src="post.imageUrl">
-        <q-card-section>
-          <div>{{post.caption}}</div>
-          <div class="text-caption text-white">{{post.date | niceDate}}</div>
-          <div class="wrapper absolute-bottom-right">
-            <q-btn flat round icon="favorite" />
-            <q-btn flat round icon="bookmark" />
-            <q-btn flat round icon="share" />
-          </div>
-        </q-card-section>
+    <q-card 
+      v-for="post in posts"
+      :key="post.id"
+      class="card-post bg-pink-4 q-mb-md"
+      flat
+      bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label
+          class="text-bold text-white">valensimip</q-item-label>
+          <q-item-label caption>
+            {{post.location}}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <img :src="post.imageUrl">
+      <q-card-section>
+        <div>{{post.caption}}</div>
+        <div class="text-caption text-white">{{post.date | niceDate}}</div>
+        <div class="wrapper absolute-bottom-right">
+          <q-btn flat round icon="favorite" />
+          <q-btn flat round icon="bookmark" />
+          <q-btn flat round icon="share" />
+        </div>
+
+      </q-card-section>
+
     </q-card>
-      </div>
-      <div class="col-4 large-screen-only">
-        <q-item class="fixed">
-          <q-item-section avatar>
-            <q-avatar  size="48px">
-              <img src="https://sun9-48.userapi.com/impg/If3R-LcLYWlkmKukSnxnCFUm83Cf-aQm3ZDKCA/CElOBgUC0CU.jpg?size=1400x1936&quality=95&sign=a9a9f900f90dca64c7bab48e9786f66a&type=album">
-            </q-avatar>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label
-              class="text-bold text-pink-11">valensimip</q-item-label>
-            <q-item-label caption>
-              Valentina
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-      </div>
-    </div>
   </q-page>
 </template>
 
@@ -106,15 +91,9 @@ export default {
 }
 </script>
 <style lang="sass">
-  .card-post
-    .q-btn 
-      color: white
-    .q-img
-      min-height: 250px
-  .q-card > img
-    max-height: 600px
-    object-fit: cover
-  .q-avatar
-    img
-      object-fit: cover
+.card-post
+  .q-btn 
+    color: white
+  .q-img
+    min-height: 250px
 </style>
