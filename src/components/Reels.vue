ad11c06cc7d880e1d4bc0cb568ad652743866cc2
Valensi <vallitriya@gmail.com>
2023-08-15 21:55:33 +0300

    Initial commit: setup video API component

---
+++ src/components/Reels.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-if="videoData">
+      <video :src="videoData.url" controls></video>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      videoData: Object, // Проп передающий данные видео из Reddit API
+    },
+  };
+  </script>
