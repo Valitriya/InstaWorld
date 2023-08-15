64a8ea776495ac6df7fa34fbc58469f2e685dc6c
Valensi <vallitriya@gmail.com>
2023-08-15 22:01:15 +0300

    Initial page template for video API component

---
+++ src/pages/PageReels.vue
@@ -0,0 +1,29 @@
+<template>
+    <q-page class="container q-pa-md">
+      <VideoComponent :videoData="videoData" />
+    </q-page>
+  </template>
+  
+  <script>
+  import VideoComponent from '../components/Reels.vue';
+  import axios from 'axios';
+  export default {
+    name: 'PageReels',
+    components: {
+    VideoComponent,
+  },
+  data() {
+    return {
+      videoData: null,
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get('URL_РЕДДИТ_API');
+      this.videoData = response.data; // Обработка данных, полученных из Reddit API
+    } catch (error) {
+      console.error('Ошибка при запросе к Reddit API', error);
+    }
+  },
+  }
+  </script>
