3312c55dfcfc913c438e9037f8d9cb3135d7f6f9
Valensi <vallitriya@gmail.com>
2023-08-22 00:18:24 +0300

    CSS modifications for fluid layout

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-page class="container q-pa-md small-screen-only">
+    <q-page class="container-more q-pa-md small-screen-only">
       <div class="camera-frame q-pa-md">
         <img class="full-width" 
              src="https://sun9-13.userapi.com/impg/gOKXlXiJu3XH7SGJvzX3TkSE0GmGS4QaTMALxA/3FpZKJBCo84.jpg?size=1080x1080&quality=96&sign=7bd8a9bf28aff869c3a2febe0d884257&type=album">
@@ -11,17 +11,17 @@
             icon="eva-camera"
             size="lg" />
       </div>
-      <div class="input-wrapper">
+      <div class="input-wrapper small-wrapper-input">
         <div class="row justify-center q-ma-md">
           <q-input 
             v-model="post.textCaption"
-            class="col custom-input text-grand-hotel"
+            class="col custom-input"
             label="Caption"/>
         </div>
         <div class="row justify-center q-ma-md">
           <q-input 
             v-model="post.textLocation "
-            class="col custom-input text-grand-hotel"
+            class="col custom-input"
             label="Location">
             <template v-slot:append>
                 <q-btn 
@@ -43,15 +43,19 @@
   </template>
   
   <script>
+  import { uid } from 'quasar'
+
   export default {
     name: 'PageCamera',
     data(){
       return{
         post: {
+          id: uid(),
           textCaption:'',
-          textLocation: ''
-        }
-        
+          textLocation: '',
+          photo: null,
+          date: Date.now()
+        } 
       }
     }
   }
@@ -65,10 +69,11 @@
       color: $white 
       font-size: 22px
       padding: 0 0 0 10px
+      font-family: 'Grand Hotel'
     .q-field__control
       --q-color-primary: #efc7f7
       background-color: $pink-11  
-      border-radius: 5px    
+      border-radius: 5px 
       &:before
         display: none
     .q-btn
